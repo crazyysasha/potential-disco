f2c3865178d5ba651bf87c2dab841dee48d03b3c
ozolox123 <lol_1717@inbox.ru>
2023-07-25 13:13:14 +0300

    ghfty

---
+++ main.js
@@ -0,0 +1,6 @@
+// let arr = ["Odilbek", "Mamayoqubov","14","malchik"]
+const odil = {name: 'Odilbek', surname: 'Mamayoqubov', age:'14', gender:'Malchik'};
+const sardor = {name: 'Sardor', surname: 'Tulkinov', age:'14', gender:'Malchik'};
+const ozod = {name: 'Ozodbek', surname: 'Mamayoqubov', age:'11', gender:'Malchik'};
+const user = [odil,sardor,ozod]
+console.log(user);
