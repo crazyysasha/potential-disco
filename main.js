21c0f47ddbb2a7aa90d24c5ad9e5bd1a3681cda7
ozolox123 <lol_1717@inbox.ru>
2023-07-25 13:17:56 +0300

    vot

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // let arr = ["Odilbek", "Mamayoqubov","14","malchik"]
-const odil = {name: 'Odilbek', surname: 'Mamayoqubov', age:'14', gender:'Malchik'};
-const sardor = {name: 'Sardor', surname: 'Tulkinov', age:'14', gender:'Malchik'};
-const ozod = {name: 'Ozodbek', surname: 'Mamayoqubov', age:'11', gender:'Malchik'};
+const odil = {name: 'Odilbek', surname: 'Mamayoqubov',email: 'lol_1717@inbox.ru' , age:'14', gender:'Malchik',Git: 'ozolox123' };
+const sardor = {name: 'Sardor', surname: 'Tulkinov', email: 'xz', age:'14', gender:'Malchik', Git: 'SARDORQUEEN'};
+const ozod = {name: 'Ozodbek', surname: 'Mamayoqubov',email: 'odilbeklezgin@gmail.com' , age:'11', gender:'Malchik', Git: 'lolopepe12' };
 const user = [odil,sardor,ozod]
 console.log(user);
