8fda8efa5412d4fabc6f810c72d83e774063785e
SARDORKING <tulkinovsardor504@gmail.com>
2023-07-27 14:31:44 +0500

    PRIVET

---
+++ main.js
@@ -0,0 +1,20 @@
+const x = '7';
+
+const y = '5';
+
+function lol(arg1, arg2){
+    console.log(arg1-arg2)
+}
+
+lol(x, y);
+
+function lolo(arg1, arg2){
+console.log( arg1*arg2)
+}
+
+lolo(x, y)
+
+function lolol(arg1, arg2){
+    console.log(arg1/arg2)
+}
+lolol(x, y)
