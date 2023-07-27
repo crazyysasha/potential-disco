add65827f16c67692eed24557d4d1facc9c08727
ozolox123 <lol_1717@inbox.ru>
2023-07-27 14:34:15 +0500

    3 homework

---
+++ main.js
@@ -0,0 +1,35 @@
+const x = 20 
+
+const y = 50
+
+
+function sum(ozo1, ozo2) {
+     console.log(+ozo1 + +ozo2);
+}
+
+sum(x, y);
+
+
+
+function zms(ozo1, ozo2) {
+     console.log(+ozo1 - +ozo2);
+}
+
+zms(x, y);
+
+
+
+function pep(ozo1, ozo2) {
+     console.log(+ozo1 * +ozo2);
+}
+
+pep(x, y);
+
+
+
+function lolo(ozo1, ozo2) {
+     console.log(+ozo1 / +ozo2);
+}
+
+lolo(x, y);
+
