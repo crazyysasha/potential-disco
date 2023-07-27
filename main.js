031dc62c8988ad1f186ddb0a71bb4e181628c55c
ozolox123 <lol_1717@inbox.ru>
2023-07-27 12:20:45 +0300

    Odilbek Classwork

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-// let arr = ["Odilbek", "Mamayoqubov","14","malchik"]
-const odil = {name: 'Odilbek', surname: 'Mamayoqubov',email: 'lol_1717@inbox.ru' , age:'14', gender:'Malchik',Git: 'ozolox123' };
-const sardor = {name: 'Sardor', surname: 'Tulkinov', email: 'xz', age:'14', gender:'Malchik', Git: 'SARDORQUEEN'};
-const ozod = {name: 'Ozodbek', surname: 'Mamayoqubov',email: 'odilbeklezgin@gmail.com' , age:'11', gender:'Malchik', Git: 'lolopepe12' };
-const user = [odil,sardor,ozod]
-console.log(user);
+const x = 51
+
+const y = 88
+
+function lol(org1, org2) {
+    console.log(+org1 + +org2 );
+}
+
+lol(x,y);
+
+function siu(org1, org2) {
+    console.log(+org1 * +org2 );
+}
+
+siu(x,y)
+
+function loll(org1, org2) {
+    console.log(+org1 / +org2 );
+}
+
+loll(x,y)
+
+function llol(org1, org2) {
+    console.log(+org1 - +org2 );
+}
+
+llol(x,y)
