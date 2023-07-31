3c3d77299869472fc9d90b9c3f2d310fc07d5cf2
Sasha Raimov <crazyysasha@gmail.com>
2023-07-31 15:00:05 +0500

    изучили события

--- main.js
+++ main.js
@@ -1,86 +1,30 @@
-const x = '5';
+//document, window, navigator
 
-const y = '8';
+console.dir(document.body.clientHeight);
 
-// console.log(+x + +y);
+// document.body.addEventListener('click', function ({ target }) {
+//     console.log(target);
+// });
 
-function sum(arg1, arg2) {
-    console.log(+arg1 + +arg2);
+
+document.body.addEventListener('click', function (event) {
+    console.log(event.target);
+});
+
+const button = document.querySelector('button');
+
+function onClickHandler(event) {
+    event.stopPropagation();
+    console.log('event on button: ', event.target);
+    console.log(event.clientX, event.clientY);
+    // button.removeEventListener('click', onClickHandler);
 }
+button.addEventListener('click', onClickHandler);
 
-sum(x, y);
+const paragraphs = document.querySelectorAll('p');
 
-sum(54, '23');
+console.log(paragraphs);
 
-const minus = (arg1, arg2) => {
-    console.log(+arg1 - +arg2);
-}
+const sardorP = document.getElementsByClassName('sardor');
 
-
-minus(54, 34);
-
-
-
-class User {
-    name;
-    birthyear;
-    getAge() {
-        console.log(this.name + "u " + (2023 - this.birthyear) + " let");
-    }
-    constructor(n, b) {
-        this.name = n;
-        this.birthyear = b;
-    }
-}
-
-const sardorkin = new User("Sardor", 2009);
-
-
-sardorkin.getAge();
-
-console.log(sardorkin.name);
-
-const odil = new User('Ozodbek', 2012);
-
-odil.getAge();
-
-
-
-const shkola = { name: '10-shkola', rasstoyaniye: 1.5 };
-
-const iceberg = { name: '...', rasstoyaniye: 3.45 }
-
-
-function sum(shkola, iceberg) {
-    console.log(shkola.rasstoyaniye + iceberg.rasstoyaniye);
-}
-
-
-
-class Product {
-    name;
-    price;
-    constructor(n, p) {
-        this.name = n;
-        this.price = p;
-    }
-}
-
-const hp = new Product('HP noutbook', 1000);
-
-const iphone = new Product('Iphone XR', 800);
-
-const galaxy = new Product('Galaxy s23 ultra', 1400);
-
-
-function sumPrice(p1, p2, p3) {
-    console.log(p1.price + p2.price + p3.price);
-}
-
-sumPrice(hp, iphone, galaxy);
-
-// TODO: #2 Car: name, mileage
-//
-//  totalMileage
-//
-//  const tesla, bmw, nexia3
+console.log(sardorP);
