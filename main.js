6055095603d00e85c33d1ae104f94d2dc08839d7
Sasha Raimov <crazyysasha@gmail.com>
2023-08-01 15:58:41 +0500

    fix

--- main.js
+++ main.js
@@ -1,90 +1,17 @@
-const canvas = document.querySelector('#canvas');
+// document, window, navigator
 
 
-console.log(window);
-function onSetSizesToCanvas() {
-    canvas.height = 4 * window.innerHeight;
-    canvas.width = 4 * window.innerWidth;
+
+document.onclick = function () {
+    console.log('clicked to document');
 }
 
-onSetSizesToCanvas();
-
-const ctx = canvas.getContext('2d');
-window.addEventListener('resize', onSetSizesToCanvas);
-
-let lastX, lastY;
-
-let isDrawing, isFlat = false;
-
-document.addEventListener('keydown', function (event) {
-    if (event.ctrlKey) {
-        isFlat = true;
-    }
-});
-document.addEventListener('keyup', function (event) {
-   
-    if (event.keyCode == 17) {
-        isFlat = false;
-    }
-});
-
-canvas.addEventListener('mousedown', function () {
-    isDrawing = true;
-});
-
-canvas.addEventListener('mouseup', function () {
-    isDrawing = false;
-});
+document.onclick = function () {
+    console.log('clicked to document 2');
+    alert('ty najal na document')
+}
 
 
-let flatStartX, flatStartY, axis;
+console.log(window)
 
-
-canvas.addEventListener('mousemove', function (event) {
-    console.log(flatStartX, flatStartY, axis);
-    if (isDrawing) {
-
-        if (lastX && lastY) {
-
-            if (isFlat) {
-
-                if (!flatStartX && !flatStartY) {
-                    flatStartX = event.clientX;
-                    flatStartY = event.clientY;
-                }
-                console.log(flatStartX, flatStartY);
-
-                ctx.moveTo(flatStartX, flatStartY);
-
-
-                if (flatStartX - event.clientX < flatStartY - event.clientY) {
-                    axis = 'y';
-                } else {
-                    axis = 'x';
-                }
-                if (axis == 'x') {
-                    ctx.lineTo(flatStartX, 4 * event.clientY);
-                } else if (axis == 'y') {
-                    ctx.lineTo(4 * event.clientX, flatStartY);
-
-                }
-            } else {
-                flatStartX = null;
-                flatStartY = null;
-                axis = null;
-                ctx.moveTo(lastX, lastY);
-                ctx.lineTo(4 * event.clientX, 4 * event.clientY);
-
-            }
-            ctx.stroke();
-            ctx.lineWidth = 10;
-        }
-    }
-    lastX = 4 * event.clientX;
-    lastY = 4 * event.clientY;
-});
-
-canvas.addEventListener('dblclick', function () {
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
+console.log(navigator);
