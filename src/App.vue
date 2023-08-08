b7c1f65b8dce35ad569944515a97233aa265438a
Sasha Raimov <crazyysasha@gmail.com>
2023-08-08 16:04:54 +0500

    fix lesson

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+import Decrement from './components/Decrement.vue';
+
+const count = ref(0);
+</script>
+
+<template>
+  <div>
+    hello world
+    <button @dblclick="count--" :style="{fontSize: `${0-count}px`}">
+      count is {{ count }}
+    </button>
+
+    <Decrement>
+    </Decrement>
+  </div>
+</template>
---
+++ src/components/Decrement.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+    <div>
+        <button v-on:click="count--">
+            count is {{ count }}
+        </button>
+    </div>
+</template>
